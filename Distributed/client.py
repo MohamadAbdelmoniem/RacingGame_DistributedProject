9c59cc5e717271caf22134141dce21e6b16b9c38
Karim Ashraf <karimashraf0055@gmail.com>
2023-06-13 04:09:41 +0300

    bug fixed and lobby created

--- Distributed/client.py
+++ Distributed/client.py
@@ -97,30 +97,30 @@ def play():
                 pygame.quit()
 
         # moving player's car using left and right arrow keys
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and player.rect.center[0] > left_lane:
-                player.rect.x -= 10
-            elif event.key == pygame.K_RIGHT and player.rect.center[0] < right_lane:
-                player.rect.x += 10
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT and player.rect.center[0] > left_lane:
+                    player.rect.x -= 10
+                elif event.key == pygame.K_RIGHT and player.rect.center[0] < right_lane:
+                    player.rect.x += 10
 
-            # check if there is a side swipe collision after changing the lane
-            for vehicle in vehicle_group:
-                if pygame.sprite.collide_rect(player, vehicle):
-                    game_over = True
-                    # place the player's car next to the other vehicle and
-                    # determine where to position the crash image
-                    if event.key == K_LEFT:
-                        player.rect.left = vehicle.rect.right
-                        crash_rect.center = [
-                            player.rect.left,
-                            (player.rect.center[1] + vehicle.rect.center[1]) / 2,
-                        ]
-                    elif event.key == K_RIGHT:
-                        player.rect.right = vehicle.rect.left
-                        crash_rect.center = [
-                            player.rect.right,
-                            (player.rect.center[1] + vehicle.rect.center[1]) / 2,
-                        ]
+                # check if there is a side swipe collision after changing the lane
+                for vehicle in vehicle_group:
+                    if pygame.sprite.collide_rect(player, vehicle):
+                        game_over = True
+                        # place the player's car next to the other vehicle and
+                        # determine where to position the crash image
+                        if event.key == K_LEFT:
+                            player.rect.left = vehicle.rect.right
+                            crash_rect.center = [
+                                player.rect.left,
+                                (player.rect.center[1] + vehicle.rect.center[1]) / 2,
+                            ]
+                        elif event.key == K_RIGHT:
+                            player.rect.right = vehicle.rect.left
+                            crash_rect.center = [
+                                player.rect.right,
+                                (player.rect.center[1] + vehicle.rect.center[1]) / 2,
+                            ]
 
         # draw the grass
         screen.fill(green)

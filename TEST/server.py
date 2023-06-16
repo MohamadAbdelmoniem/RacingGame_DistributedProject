fdcf1b6f5515fb3db0a4b06dbaece6a1c31e656e
Fathy24 <93041833+Fathy24@users.noreply.github.com>
2023-06-16 17:55:34 +0300

    opponent display & chat test

--- TEST/server.py
+++ TEST/server.py
@@ -19,14 +19,28 @@ scores = {"player1": 0, "player2": 0}
 positions = {"player1": (330,400), "player2": (330,400)}
 clients = []
 
-def broadcast(scores):
+def broadcast(msg):
     for client in clients:
-        client.sendall(pickle.dumps(scores))
+        client.sendall(pickle.dumps(msg))
+
+def chat_handler(conn):
+    while True:
+        try:
+            chat_msg = pickle.loads(conn.recv(2048))
+            if not chat_msg:
+                break
+            if chat_msg.get('chat'):
+                broadcast(chat_msg)
+        except:
+            break
 
 def threaded_client(conn, player_id):
     print("server will send id: ", player_id)
     conn.send(pickle.dumps(player_id))
     print("server sent id: ", player_id)
+
+    chat_thread = threading.Thread(target=chat_handler, args=(conn,))
+    chat_thread.start()
 
     while True:
         try:
